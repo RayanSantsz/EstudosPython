5d27e6f9f6dd4ef9b1dffb0e1d7748fa487ebe9e
Rayan Santos <rayansantos0088@gmail.com>
2025-09-15 16:07:11 -0300

    Modulos

---
+++ Modulos/main.py
@@ -0,0 +1,13 @@
+# Programa principal
+import math_operations
+from math_operations import multiply, divide
+import string_utils
+
+print(math_operations.sum(5,3))
+print(math_operations.subtract(5,3))
+print(multiply(5,3))
+print(f"{divide(5,3):.2f}")
+
+print(string_utils.capitalize("heloo"))
+print(string_utils.reverse_string("Python"))
+print(string_utils.count("Apple"))
