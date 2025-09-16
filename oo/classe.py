d679c6b6d78d1477cfafe386d202f02ad01b8518
Rayan Santos <rayansantos0088@gmail.com>
2025-09-16 16:17:07 -0300

    começo de oo

---
+++ oo/classe.py
@@ -0,0 +1,5 @@
+class Game:
+    name = ''
+    yearLauch = ''
+    multiplayer = False
+    note = 0
---
+++ oo/objetos.py
@@ -0,0 +1,17 @@
+class Game:
+    name = ''
+    yearLauch = ''
+    multiplayer = False
+    note = 0
+
+# Primeiro jogo
+game1 = Game()
+game1.name = 'The Legend of Zelda: Breath od the Wild'
+game1.yearLauch = 2017
+game1.multiplayer = False
+game1.note = 9.5
+
+# Segundo jogo
+
+print("###Dados do Jogo###")
+print(f'Nome do jogo: {game1.name}\nAno de lançamento: {game1.yearLauch} ')
