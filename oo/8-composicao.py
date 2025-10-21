ec8537c745c114da2d54681827f82a3e660475a5
Rayan Santos <rayansantos0088@gmail.com>
2025-10-21 17:30:15 -0300

    adicionei a composicao

--- oo/8-composicao.py
+++ oo/8-composicao.py
@@ -16,3 +16,34 @@ class Game:
         print(f'Ano de lançamento: {self.yearLaunch}')
         print(f'Modo multiplayer: {self.multiplayer}')
         print(f'Avaliação do jogo: {self.note}\n')
+
+class GameStudio:
+    def __init__(self,name=""):
+        self.name = name
+        self.games = []
+
+    def add_game(self,game):
+        self.games.append(game)
+
+    def evaluate_studio_quality(self):
+        total_notes = sum(game.note for game in self.games)
+        num_games = len(self.games)
+        if num_games == 0:
+            print(f'O estúdio {self.name} ainda não lançou jogo')
+        else:
+            average_note = total_notes / num_games
+            print(f'Avaliação média dos jogos do estúdio {self.name}: {average_note:.2f}')
+
+game1 = Game('The Legend od Zelda', 2017, False, 9.5)
+game2 = Game('Fortnite', 2017,True,8.0)
+game3 = Game('The Last of Us II', 2020, False, 9.0)
+
+studio = GameStudio('Awesome Games')
+studio.add_game(game1)
+studio.add_game(game2)
+studio.add_game(game3)
+
+for game in studio.games:
+    game.technical_sheet()
+
+studio.evaluate_studio_quality()
